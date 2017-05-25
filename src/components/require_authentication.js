b1fdfc3c48cc496e850fae91fffcbc51c625e7b8
jpca999 <jpca999@gmail.com>
2017-05-25 18:57:44 -0400

    45 mid

--- src/components/require_authentication.js
+++ src/components/require_authentication.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 export default function (ComposedComponent) {
   class Authentication extends Component {
-    render(){
+    static contextTypes = {
+      router: React.PropTypes.object
+    }
+    componentWillMount() {
+      if(!this.props.authenticated){
+      this.context.router.push('/')
+      }
+      
+    }
+
+    render() {
+      console.log(this.context);
       return <ComposedComponent {...this.props} />
     }
   }
 
-return Authentication;
+function mapStateToProps(state) {
+    return { authenticated: state.authenticated };
+}
+return connect(mapStateToProps)(Authentication);
 
 }
 
